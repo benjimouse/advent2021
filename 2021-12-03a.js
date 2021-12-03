814559550a9216445dd2929c7587bfb2db36e2bc
Ben Best <ben.best@olmgroup.com>
2021-12-03 09:48:12 +0000

    Day 3 part 1

--- 2021-12-03a.js
+++ 2021-12-03a.js
@@ -9,21 +9,13 @@ function ben(inputFile) {
     const diagnostic = getArray(inputFile);
     let gamma1, gamma2, gamma3, gamma4, gamma5, epsilon1, epsilon2, epsilon3, epsilon4, epsilon5;
     let pos = [];
-    pos[0] = 0;
-    pos[1] = 0;
-    pos[2] = 0;
-    pos[3] = 0;
-    pos[4] = 0;
     const arrSize = diagnostic.length;
 
     for (let i = 0; i < arrSize; i++) {
         line = diagnostic[i].split('');
-
-        for(let j=0;j<line.length;j++){
-            pos[j] = pos[j] + parseInt(line[j]);
+        for (let j = 0; j < line.length; j++) {
+            pos[j] = isNaN(pos[j]) ? 0 : pos[j] + parseInt(line[j]);
         }
-        
-        
     }
     const gamma = posToBin(pos, arrSize, 'g');
     const epsilon = posToBin(pos, arrSize, 'e');
@@ -32,18 +24,17 @@ function ben(inputFile) {
 
 function posToBin(pos, arrSize, gOrE) {
     let bin = [];
-    for(let i=0;i<pos.length;i++){
+    for (let i = 0; i < pos.length; i++) {
         if (gOrE === 'g') {
             bin[i] = pos[i] > (arrSize / 2) ? '1' : '0';
         } else {
             bin[i] = pos[i] > (arrSize / 2) ? '0' : '1';
         }
     }
-    binaryNum = bin.join('');
-
+    const binaryNum = bin.join('');
     const gamma = parseInt(binaryNum, 2);
-
     return gamma;
 }
 
-console.log(ben("./2021-12-03-test.in")===198?"Success":"Failure "+ben("./2021-12-03-test.in"));
+console.log(ben("./2021-12-03-test.in") === 198 ? "Success" : "Failure " + ben("./2021-12-03-test.in"));
+console.log(ben('./2021-12-03.in'));
